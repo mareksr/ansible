a5ce939463a1116771a257923efb1e3e5cf28a00
root <root@nagios.hb.pl>
2016-04-13 15:23:51 +0200

    ok

--- xweb/update_php52.sh
+++ xweb/update_php52.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ansible-playbook    -b --become-method=su --ask-become-pass --become-user=root -e "pipelining=False" php/php5.2.yml --limit 178.33.174.199
+ansible-playbook    -b --become-method=su --ask-become-pass --become-user=root -e "pipelining=False" php/php5.2.yml --limit 178.33.174.199 
+#ansible-playbook    -b --become-method=su --ask-become-pass --become-user=root -e "pipelining=False" php/php5.2.yml 
 
